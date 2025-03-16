094783144689b207be830daaaabf5c79a6f33ebf
agachiu2002 <agachiu2002@gmail.com>
2025-03-16 04:50:36 +0000

    modify

--- lesson8/lesson8_3.py
+++ lesson8/lesson8_3.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-def main():
     df = pd.read_csv('上市公司資料.csv')
     df1 = df.dropna()
     df2 = df1.reindex(columns=['公司代號','出表日期','公司名稱','產業別','營業收入-當月營收','營業收入-上月營收'])
@@ -8,7 +7,10 @@ def main():
         '營業收入-上月營收':'上月營收',
         '營業收入-當月營收':'當月營收'
         })
-    df3.to_csv('上市公司資料整理.csv',encoding='utf-8')
-    df3.to_excel('上市公司資料整理.xlsx')
-    print("存檔完成")
+    print(df3)
+
+df3.to_csv('上市公司整理.csv',encoding='utf-8')
+df3.to_excel('上市公司整理.xlsx',encoding='utf-8')
+print('存檔完成')
+
 
